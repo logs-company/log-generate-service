bd019a6aab277fbbbfc6da909701483bcaf14f2f
Math012 <math111.0811@gmail.com>
2025-09-02 16:26:11 -0300

    Adicionando a classe de serviço para enviar o tópico send log via Kafka

---
+++ src/main/java/com/math12/log_generate_service/business/service/SendLogService.java
@@ -0,0 +1,17 @@
+package com.math12.log_generate_service.business.service;
+
+import com.math12.log_generate_service.business.topics.kafka.producer.SendLogProducer;
+import generatelogs.GenerateLog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SendLogService {
+
+    @Autowired
+    private SendLogProducer producer;
+
+    public void sendLogForTopic(){
+        producer.sendEventLog(GenerateLog.getLog());
+    }
+}
