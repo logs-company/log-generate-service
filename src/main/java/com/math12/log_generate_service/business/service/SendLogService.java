18caddad41a2c8bcbe9511b55b8eeef53a950920
Math012 <math111.0811@gmail.com>
2025-09-03 10:46:00 -0300

    Adicionando o Scheduled ao método que envia topicos

--- src/main/java/com/math12/log_generate_service/business/service/SendLogService.java
+++ src/main/java/com/math12/log_generate_service/business/service/SendLogService.java
@@ -3,6 +3,7 @@ package com.math12.log_generate_service.business.service;
 import com.math12.log_generate_service.business.topics.kafka.producer.SendLogProducer;
 import generatelogs.GenerateLog;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class SendLogService {
     @Autowired
     private SendLogProducer producer;
 
+    @Scheduled(fixedRate = 5000)
     public void sendLogForTopic(){
         producer.sendEventLog(GenerateLog.getLog());
     }
